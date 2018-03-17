88b69f9a917cad1a21f84614823b7c2aff8fda29
Xingda Wang <wxd598113636@gmail.com>
2018-03-17 00:38:57 +0000

    update action name and add new action on calculate BFP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
-export const calculateHealth = (data) => {
+export const calculateBMI = (healthCondition) => {
 	return {
 		type: "CALCULATE_BMI",
-		payload: data
+		payload: healthCondition
+	}
+}
+
+export const calculateBFP = (healthCondition) => {
+	return {
+		type: "CALCULATE_BFP",
+		payload: healthCondition
 	}
 }
