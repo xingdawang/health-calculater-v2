f6185a4ebf6db595b854490d391e83c2cf595845
Xingda Wang <wxd598113636@gmail.com>
2018-03-17 03:28:36 +0000

    update homepage content and style

--- src/components/home-content.js
+++ src/components/home-content.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 class HomeContent extends Component {
   render() {
     return (
-      <div className="home-page-content">
-        <h1 className="home-page-content-title"> Lose weight! </h1>
+      <div className="home-page-content jumbotron">
+        <h1 className="home-page-content-title text-center"> Lose weight! </h1>
+        <p className="text-justify">You know the drill when it comes to losing weight: take in fewer calories,
+         burn more calories. But you also know that most diets and quick weight-loss 
+         plans don't work as promised. If you're trying to drop a few pounds fast, 
+         these trusted expert tools will make it easy for you to lose the weight quickly.
+        </p>
       </div>
     )
   }
