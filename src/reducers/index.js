fe6cb862e9619c59ce0b62456c32cb7b302e278e
Xingda Wang <wxd598113636@gmail.com>
2018-03-17 00:39:42 +0000

    remove test reducer (test reducer)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import healthConditionReducer from './health-calculator'
-import defaultConditionReducer from './default-condition'
 
 const rootReducer = combineReducers({
-	defaultCondition: defaultConditionReducer,
 	healthCondition: healthConditionReducer,
 	form: formReducer
 })
