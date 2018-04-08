9ae9673741297beb676b15a50e44bb2b483049bc
Xingda Wang <wxd598113636@gmail.com>
2018-04-08 10:18:40 +0200

    update bmi and bfp page styles

--- src/components/bfp-indicator.js
+++ src/components/bfp-indicator.js
@@ -6,62 +6,90 @@ class BFPIndicator extends Component {
 
 	render() {
 		return (
-			<div>
-				<h3><span className="label label-primary"> Your height </span>: {this.props.condition.height} cm</h3>
-				<h3><span className="label label-primary"> Your weight </span>: {this.props.condition.weight} Kg</h3>
-				<h3><span className="label label-primary"> Your gender </span>: {this.props.condition.gender} </h3>
-				<h3><span className="label label-primary"> Your birthdate </span>: {this.props.condition.dateOfBirth} </h3>
-				<h3><span className="label label-primary"> Your BFP </span>: {this.props.condition.bfp}</h3>
-
-				{this.props.condition.gender === "male" ? 
-					<Chart
-						chartType="Gauge"
-						data={[
-							['Label', 'Value'],
-							['BFP', Number(this.props.condition.bfp) ]
-						]}
-						options={{
-							redFrom: 29, redTo: 45,
-							yellowFrom: 22, yellowTo: 29,
-							greenFrom: 0, greenTo: 22,
-							majorTicks : ['0','4','8','12','16','20','24','28','32','36'],
-							minorTicks: 4,
-							animation:{
-								duration: 1000,
-								easing: 'inAndOut'
-							},
-							max: 36, min: 0
-						}}
-						graph_id="GaugeChart"
-						width="100%"
-						height="300px"
-						legend_toggle
-					/>
-					:
-					<Chart
-						chartType="Gauge"
-						data={[
-							['Label', 'Value'],
-							['BFP', Number(this.props.condition.bfp) ]
-						]}
-						options={{
-							redFrom: 39, redTo: 55,
-							yellowFrom: 31, yellowTo: 39,
-							greenFrom: 5, greenTo: 31,
-							majorTicks : ['10','15','20','25','30','35','40','45','50'], 
-							minorTicks: 5,
-							animation:{
-								duration: 1000,
-								easing: 'inAndOut'
-							},
-							max: 50, min: 10
-						}}
-						graph_id="GaugeChart"
-						width="100%"
-						height="300px"
-						legend_toggle
-					/>
-				}
+			<div className="col-md-12">
+				<div className="col-md-6">
+					<table className="bmi-table table table-hover table-bordered">
+						<thead>
+							<tr>
+								<th>Your Information</th>
+								<th>Value</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><span className="label label-primary"> Your height </span></td>
+								<td>{this.props.condition.height} cm</td>
+							</tr>
+							<tr>
+								<td><span className="label label-primary"> Your weight </span></td>
+								<td>{this.props.condition.weight} Kg</td>
+							</tr>
+							<tr>
+								<td><span className="label label-primary"> Your gender </span></td>
+								<td>{this.props.condition.gender} </td>
+							</tr>
+							<tr>
+								<td><span className="label label-primary"> Your birthdate </span></td>
+								<td>{this.props.condition.dateOfBirth} </td>
+							</tr>
+							<tr>
+								<td><span className="label label-primary"> Your BFP </span></td>
+								<td>{this.props.condition.bfp}</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div className="col-md-6">
+					{this.props.condition.gender === "male" ? 
+						<Chart
+							chartType="Gauge"
+							data={[
+								['Label', 'Value'],
+								['BFP', Number(this.props.condition.bfp) ]
+							]}
+							options={{
+								redFrom: 29, redTo: 45,
+								yellowFrom: 22, yellowTo: 29,
+								greenFrom: 0, greenTo: 22,
+								majorTicks : ['0','4','8','12','16','20','24','28','32','36'],
+								minorTicks: 4,
+								animation:{
+									duration: 1000,
+									easing: 'inAndOut'
+								},
+								max: 36, min: 0
+							}}
+							graph_id="GaugeChart"
+							width="100%"
+							height="300px"
+							legend_toggle
+						/>
+						:
+						<Chart
+							chartType="Gauge"
+							data={[
+								['Label', 'Value'],
+								['BFP', Number(this.props.condition.bfp) ]
+							]}
+							options={{
+								redFrom: 39, redTo: 55,
+								yellowFrom: 31, yellowTo: 39,
+								greenFrom: 5, greenTo: 31,
+								majorTicks : ['10','15','20','25','30','35','40','45','50'], 
+								minorTicks: 5,
+								animation:{
+									duration: 1000,
+									easing: 'inAndOut'
+								},
+								max: 50, min: 10
+							}}
+							graph_id="GaugeChart"
+							width="100%"
+							height="300px"
+							legend_toggle
+						/>
+					}
+				</div>
 			</div>
 		)
 	}
--- src/components/bmi-indicator.js
+++ src/components/bmi-indicator.js
@@ -7,36 +7,57 @@ class BMIIndicator extends Component {
 	render() {
 		return (
 			
-			<div className="col-md-6">
+			<div className="col-md-12">
 				<div className="col-md-6">
-					<h3><span className="label label-primary"> Your height </span>: {this.props.condition.height} cm</h3>
-					<h3><span className="label label-primary"> Your weight </span>: {this.props.condition.weight} Kg</h3>
-					<h3><span className="label label-primary"> Your BMI </span>: {this.props.condition.bmi}</h3>
+					<table className="bmi-table table table-hover table-bordered">
+						<thead>
+							<tr>
+								<th>Your Information</th>
+								<th>Value</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><span className="label label-primary"> Your height </span></td>
+								<td>{this.props.condition.height} cm</td>
+							</tr>
+							<tr>
+								<td><span className="label label-primary"> Your weight </span></td>
+								<td>{this.props.condition.weight} Kg</td>
+							</tr>
+							<tr>
+								<td><span className="label label-primary"> Your BMI </span></td>
+								<td>{this.props.condition.bmi}</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 
-				<Chart
-					chartType="Gauge"
-					data={[
-						['Label', 'Value'],
-						['BMI', Number(this.props.condition.bmi) ]
-					]}
-					options={{
-						redFrom: 35, redTo: 55,
-						yellowFrom: 25, yellowTo: 35,
-						greenFrom: 10, greenTo: 25,
-						majorTicks : ['15', '20','25','30','35','40','45','50'],
-						minorTicks: 5,
-						animation:{
-							duration: 1000,
-							easing: 'inAndOut'
-						},
-						max: 50, min: 15
-					}}
-					graph_id="GaugeChart"
-					width="100%"
-					height="300px"
-					legend_toggle
-				/>
+				<div className="col-md-6">
+					<Chart
+						chartType="Gauge"
+						data={[
+							['Label', 'Value'],
+							['BMI', Number(this.props.condition.bmi) ]
+						]}
+						options={{
+							redFrom: 35, redTo: 55,
+							yellowFrom: 25, yellowTo: 35,
+							greenFrom: 10, greenTo: 25,
+							majorTicks : ['15', '20','25','30','35','40','45','50'],
+							minorTicks: 5,
+							animation:{
+								duration: 1000,
+								easing: 'inAndOut'
+							},
+							max: 50, min: 15
+						}}
+						graph_id="GaugeChart"
+						width="100%"
+						height="300px"
+						legend_toggle
+					/>
+				</div>
 			</div>
 		)
 	}
